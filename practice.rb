852af39997d2412962dea5983e5c2d02d7a8f024
Cosmel Villalobos <cosmelvg@gmail.com>
2020-10-07 13:28:37 -0400

    adding my first ruby file

---
+++ practice.rb
@@ -0,0 +1,15 @@
+name =  "cosmel "  
+print name.upcase
+
+lastname = "VILLALOBOS "
+print lastname.downcase
+
+capitalize = "very tall building"
+print capitalize.capitalize
+
+
+print "cosmel".capitalize
+print "cosmel".upcase
+print "COSMEL".downcase
+
+print "cosmel ".capitalize + "villalobos".upcase
